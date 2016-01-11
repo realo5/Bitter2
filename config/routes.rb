10f961064087ac36674991b6b707d60d2858f725
Raf Richards <nightbreeze714@hotmail.com>
2016-01-11 15:40:38 -0500

    Messages controller and database migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome' => 'pages#home'
+  get '/messages' => 'messages#index'
+  get 'messages/new' => 'messages#new'
+  
+  post 'messages' => 'messages#create'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
